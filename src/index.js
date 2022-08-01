fbae7d5332c71fffbc9f4b30c22e1681a6227c61
Artem Mokhov <teumik@gmail.com>
2022-08-01 12:48:00 +0400

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, conf) {
+  let arr = [];
+  let obj = {};
+  let rev = {};
+
+  conf.forEach((el, i, array) => {
+    obj[el[0]] = el[1];
+  });
+
+  conf.forEach((el, i, array) => {
+    rev[el[1]] = el[0];
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let last = arr[arr.length - 1];
+
+    if (obj[str[i]] === rev[str[i]]) {
+      arr.push(str[i]);
+
+      if (arr.length > 1 && rev[str[i]] === last) {
+        arr.pop();
+        arr.pop();
+      }
+    } else {
+      if (obj[str[i]]) {
+        arr.push(str[i]);
+      } else {
+        if (rev[str[i]] === last) {
+          arr.pop();
+        } else {
+          arr.push(str[i]);
+        }
+      }
+    }
+  }
+
+  return arr.length === 0;
 }
